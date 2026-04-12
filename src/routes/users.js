393e3dc3b7ee1a674a99f7e1dd9c7f483a33491c
Jashan <singhjagjit23447@gmail.com>
2026-04-12 12:46:47 +0530

    new the verify api is added

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,7 +6,15 @@ router.get('/', (req, res) => {
     res.send(' all users');
 });
 
-// GET single user
+
+router.get('Verify', (req, res) => {
+   // first get the user id of the firebase 
+   // then check if the user id exists in the database
+   // if it exists then make id in the postgress database and return the user id to the frontend
+   // if the user id does not exist in the database then return an error message to the frontend
+   // if the user id of the firebase exists in the postgress then return the user id to the frontend and do not create a new user in the postgress database
+});
+    // GET single user
 router.get('/:id', (req, res) => {
     res.send(`User ID: ${req.params.id}`);
 });
