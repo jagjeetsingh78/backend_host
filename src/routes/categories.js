9669b99184b10971b372fcfdda2b4c20be40c721
Jashan <singhjagjit23447@gmail.com>
2026-04-13 14:13:58 +0530

    env removerd

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -6,19 +6,7 @@ router.get('/', (req, res) => {
     res.send(' all categories');
 });
 
-// router.get('/check', (req, res) => {
-//     const { sql } = require('../db');
-//     sql`
-//         SELECT * FROM exams;
-//     `
-//     .then(result => {
-//         res.json(result);
-//     })
-//     .catch(error => {
-//         console.error('Error fetching categories:', error);
-//         res.status(500).json({ error: 'Failed to fetch categories' });
-//     });
-// });
+
 
 
 
