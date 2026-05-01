90279f5fe528c930be002c12616538396ff16b8f
Jagjit Singh <139382090+jagjeetsingh78@users.noreply.github.com>
2026-05-01 17:06:06 +0530

    sql url removed

--- test2.js
+++ test2.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "postgresql://postgres:vBdNrgRmiReRrKVBgUJBxXztbANKVGfJ@hopper.proxy.rlwy.net:46781/railway",
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
